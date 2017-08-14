3ea2227dbea2361194034d64061f3138c0ad3dba
Nnyanzi Ian <nnyanziian@gmail.com>
2017-08-14 17:13:29 +0300

    get post by id is done

--- server.js
+++ server.js
@@ -3,52 +3,91 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongoDB = "mongodb://127.0.0.1:27017/fschat";
 const app = express();
-
 mongoose.connect(mongoDB, {
     useMongoClient: true,
-  });
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+});
 
 app.use(bodyParser.json());
 
-const db=mongoose.connection;
-mongoose.Promise = global.Promise;
-db.on('error', console.error.bind(console, " Mongo DB conection error:"));
+function handlerError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({
+        "error": message,
+    });
+}
 
-    console.log("Database Connection ready");
-
-    function handlerError(res, reason, message, code) {
-        console.log("ERROR: " + reason);
-        res.status(code || 500).json({
-            "error": message
+function validateInput(eInput, element) {
+    //req.body.username
+    if (!eInput) {
+        // console.log("Invalid user input", "Must provide a " + element);
+        res.status(500).json({
+            "error": "Invalid user input Must provide a " + element,
         });
     }
+}
 
-app.post("/api/contacts", function (req, res) {
-    var newUser = req.body;
-    if (!req.body.name) {
-        handlerError(res, "Invalid user input", "Must provide a name.", 400);
-    }
-    db.collection('users').inserOne(newUser, function (err, doc) {
+var userSchema = mongoose.Schema({
+    "username": String,
+    "userPassword": String,
+    "userStatus": String,
+    "online": Boolean,
+    "created": {
+        type: Date,
+        default: Date.now
+    },
+});
+
+app.post("/api/users", function (req, res) {
+    //var newUser = req.body;
+    validateInput(req.body.username, "Username");
+    validateInput(req.body.userPassword, "Password");
+
+    var User = mongoose.model('users', userSchema);
+
+    var newUser = new User({
+        "username": req.body.username,
+        "userPassword": req.body.userPassword,
+        "userStatus": 'Hey I am using fschat',
+        "online": false,
+    });
+
+    newUser.save(function (err) {
         if (err) {
             handlerError(res, err.message, "Failed to create account");
         } else {
-            res.status(201).json(docs.ops[0]);
+            res.status(201).json(newUser);
         }
     });
 });
 
 app.get("/api/users", function (req, res) {
-    db.collection('users').find({}).toArray(function (err, docs) {
+    var User = mongoose.model('users', userSchema);
+    //Model.find(query, fields, options, callback)
+    User.find(({}), function (err, data) {
         if (err) {
             handlerError(res, err.message, "Failed to get users");
         } else {
-            res.status(200).json(docs);
+            res.status(200).json(data);
         }
     });
 });
 
-app.put("/api/contacts/:id", function (req, res) {
-
+app.get("/api/user/:id", function (req, res) {
+    var User = mongoose.model('users', userSchema);
+    //Model.find(query, fields, options, callback)
+    User.findById(req.params.id, function (err, data) {
+        if (err) {
+            handlerError(res, err.message, "User not Found");
+        } else {
+            res.status(200).json(data);
+        }
+    });
 });
 app.delete("/api/contacts/:id", function (req, res) {
 
